244132d6ac45086c540012fb1c08ec4be037e993
janrueggi <jan.rueggemeier@gmail.com>
2024-06-28 01:18:06 +0200

    Created simple train for parameter search

--- data/reconstruction_dataset.py
+++ data/reconstruction_dataset.py
@@ -7,7 +7,6 @@ from skimage import io, transform
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
 from tqdm import tqdm
 import psutil
 
--- neural_nets/models/unet.py
+++ neural_nets/models/unet.py
@@ -3,8 +3,75 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from neural_nets.trainer import THparams
+from neural_nets.utility_functions import Hparams
 
+class UNet3(nn.Module):
+    # UNet with adaptable depth 
+    def __init__(self, h:Hparams=None):
+        super().__init__()
+        self.depth = h.depth
+        self.channels = h.channels
 
+        # Create encoder layers
+        self.encoder = nn.ModuleList()
+        in_channels = 1
+        for i in range(self.depth):
+            out_channels = self.channels * (2 ** i)
+            self.encoder.append(self.contracting_block(in_channels, out_channels))
+            in_channels = out_channels
+
+        # Create decoder layers
+        self.decoder = nn.ModuleList()
+        for i in range(self.depth - 1, 0, -1):
+            in_channels = self.channels * (2 ** i)
+            out_channels = self.channels * (2 ** (i - 1))
+            self.decoder.append(self.expansive_block(in_channels, out_channels))
+
+        # Final expansive block and output layer
+        self.final_block = self.expansive_block(self.channels, self.channels)
+        self.final_conv = nn.Conv2d(self.channels, 1, kernel_size=1)
+
+    def contracting_block(self, in_channels, out_channels):
+        block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        return block
+
+    def expansive_block(self, in_channels, out_channels):
+        block = nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU()
+        )
+        return block
+
+    def forward(self, x):
+        # Encoder
+        enc_outputs = []
+        for encode in self.encoder:
+            x = encode(x)
+            enc_outputs.append(x)
+
+        # Bottleneck (last encoder output without pooling)
+        bottleneck = enc_outputs[-1]
+
+        # Decoder
+        for i, decode in enumerate(self.decoder):
+            if i == 0:
+                bottleneck = decode(bottleneck)
+            else:
+                bottleneck = decode(bottleneck + enc_outputs[-(i + 1)])
+
+        # Final layer
+        dec0 = self.final_block(bottleneck)
+        final = torch.sigmoid(self.final_conv(dec0))
+        return final
 
 
 class UNet2(nn.Module):
