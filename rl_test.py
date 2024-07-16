2404c33af9a67211337569f547fbf52905e6403e
daniel strauss <daniel.strauss@tum.de>
2024-07-16 16:13:57 +0200

    dummy net can be used for testing

--- rl_test.py
+++ rl_test.py
@@ -1,16 +1,33 @@
 import time
 import torch
+from stable_baselines3 import A2C
 from torch import nn
 import numpy as np
 from neural_nets.models.unet import UNet3
 from neural_nets.utility_functions import load_data
 
-from stable_baselines_code.environment import ShapeEnv
-from stable_baselines_code.reward_functions import basic_reward
 from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.logger import configure
+
 
 import pickle
 import io
+
+
+from stable_baselines_code.environment import ShapeEnv
+from stable_baselines_code.reward_functions import basic_reward
+from stable_baselines_code.example_usage_environment import DummyRecNet # importing dummy net for test purposes
+
+
+tmp_path = "./rl_runs/"
+# set up logger
+new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+
+
+# use dummy rec net to save ram, for testing
+use_dummy_rec_net = True
+
+show_example_run = False
 
 class CPU_Unpickler(pickle.Unpickler):
     def find_class(self, module, name):
@@ -20,19 +37,23 @@ class CPU_Unpickler(pickle.Unpickler):
             return super().find_class(module, name)
 
 trial_path = './neural_nets/best_trial.pkl'
-model_path = './neural_nets/trained_rec.pkl'
+rec_net_path = './neural_nets/trained_rec.pkl'
 
-with open(trial_path, 'rb') as pickle_file:
-    best_trial = CPU_Unpickler(pickle_file).load()
-pickle_file.close()
 
-with open(model_path, 'rb') as pickle_file:
-    states = CPU_Unpickler(pickle_file).load()
-pickle_file.close()
+if not use_dummy_rec_net:
+    with open(trial_path, 'rb') as pickle_file:
+        best_trial = CPU_Unpickler(pickle_file).load()
+    pickle_file.close()
 
-config = best_trial.config
-rec_net = UNet3(config)
-rec_net.load_state_dict(states['net_state_dict'])
+    with open(rec_net_path, 'rb') as pickle_file:
+        states = CPU_Unpickler(pickle_file).load()
+    pickle_file.close()
+
+    config = best_trial.config
+    rec_net = UNet3(config)
+    rec_net.load_state_dict(states['net_state_dict'])
+else:
+    rec_net = DummyRecNet()
 
 train_set, eval_set, test_set = load_data(transform=None)
 dataset = torch.utils.data.ConcatDataset([train_set, eval_set])
@@ -40,7 +61,15 @@ dataset = torch.utils.data.ConcatDataset([train_set, eval_set])
 env = ShapeEnv(rec_net, dataset, nn.BCELoss(), basic_reward)
 env.reset()
 
-while True:
+# example satble baseline model
+model = A2C("MlpPolicy", "CartPole-v1", verbose=1)
+# Set new logger
+model.set_logger(new_logger)
+model.learn(10000)
+
+# example run
+while show_example_run:
+    print("Example Run")
     action = env.action_space.sample()  # Sample random action
     observation, reward, done, info = env.step(action)
     print(reward)
