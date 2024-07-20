28d190f68dc7f29a673b0f442cb79b94686dcd0c
janrueggi <jan.rueggemeier@gmail.com>
2024-07-20 22:06:41 +0200

    performance plot

--- plots_plakat/rl_functions_performance_plot.py
+++ plots_plakat/rl_functions_performance_plot.py
@@ -1,3 +1,27 @@
+import matplotlib.pyplot as plt
+import pickle
+import os
+import time
 
-import matplotlib as plt
+save_path = 'plots_plakat/plot_data/rl_policies_statistics.pkl'
 
+with open(save_path, 'rb') as f:
+    data = pickle.load(f)
+    f.close()
+
+
+
+x = range(1, 11)
+plt.figure(figsize=(10, 6))
+
+for key, value in data.items():
+    plt.plot(x, value, label=key)
+
+plt.title('Example Plot of Different Datasets')
+plt.xlabel('Steps')
+plt.ylabel('Accuracy')
+
+plt.legend()
+plt.show()
+
+time.sleep(30)
