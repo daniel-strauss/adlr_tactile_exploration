3b7906660494fc67912346ca1946e34539f72e30
janrueggi <jan.rueggemeier@gmail.com>
2024-07-21 21:30:10 +0200

    reverted

--- rl_perform_gp.py
+++ rl_perform_gp.py
@@ -16,15 +16,12 @@ model_names = ['rew500k9', 'obs500k9', 'complex_after_free', 'diff_after_free']
 model_paths = [os.path.join('rl_models/', name) for name in model_names]
 save_path = 'plots_plakat/plot_data/rl_policies_statistics_gp.pkl'
 
-
-if debug:
-    model_names = ['daniel_o2/complex_after_free']#, 'daniel_o2/diff_after_free', 'jan/rew500k9', 'jan/obs500k9']
-    max_n = 300
+if os.path.isfile(save_path):
+    with open(save_path, 'rb') as f:
+        data = pickle.load(f)
+        f.close()
 else:
-    model_names = ['daniel_o2/complex_after_free', 'daniel_o2/diff_after_free', 'jan/rew500k9', 'jan/obs500k9']
-model_paths = [os.path.join('rl_models/', name) for name in model_names]
-save_path = 'plots_plakat/plot_data/rl_policies_statistics_gp.pkl'
-
+    data = {}
 
 rec_net = RecNet(dummy=False)
 
@@ -38,7 +35,6 @@ if skip and name in data:
     print(f'Model {name} already evaluted, skipping.')
 else:
     options = {}
-
     n = len(dataset)
     metrics = np.zeros((10, n))
     print(metrics.shape)
@@ -67,10 +63,9 @@ for name in model_paths:
     for i in tqdm.tqdm(range(n), name):
         options['index'] = i
         observation, info = env.reset(options=options)
-        while env.num_pgs() < max_grasp:
+        while env.num_pgs() < 10:
             action, _states = model.predict(observation, deterministic=False)  # Sample random action
             observation, reward, done, truncated, info = env.step(action)
-
             if not info['missed'] and not info['double']:
                 metrics[env.num_pgs()-1,i] += reward
     
