4dda3b425408169e097b668ff37b00ad57a76900
ge36quv <ge36quv@mytum.com>
2024-07-21 18:28:32 +0200

    commit for pull

--- plots_plakat/rl_functions_performance_plot.py
+++ plots_plakat/rl_functions_performance_plot.py
@@ -5,7 +5,12 @@ import time
 
 from plots_plakat.color_shemes import main_colorscheme
 
-data_path = 'plots_plakat/plot_data/rl_policies_statistics.pkl'
+x_steps = False
+
+if x_steps:
+    data_path = 'plots_plakat/plot_data/rl_policies_statistics.pkl'
+else:
+    data_path = 'plots_plakat/plot_data/rl_policies_statistics_gp.pkl'
 
 with open(data_path, 'rb') as f:
     data = pickle.load(f)
@@ -15,22 +20,31 @@ x = range(1, 11)
 plt.figure(figsize=(10, 6))
 
 for i, [key, value] in enumerate(data.items()):
-    print(f'{key}: {value[-1,0]}')
-    mean = value[:,0]
-    std = value[:,1]
+    if not key.endswith('metric'):
+        print(f'{key}: {value[-1,0]}')
+        mean = value[:,0]
+        std = value[:,1]
 
-    upper = mean + std
-    lower = mean - std
-    c = main_colorscheme[i]
-    p = plt.plot(x, mean, label=key, color=c)
-    #plt.fill_between(x, upper, lower, color=c, alpha=0.3)
+        upper = mean + std
+        lower = mean - std
+        c = main_colorscheme[i]
+        p = plt.plot(x, mean, label=key, color=c)
+        #plt.fill_between(x, upper, lower, color=c, alpha=0.3)
 
 
-plt.title('Example Plot of Different Datasets')
-plt.xlabel('Steps')
+plt.title('Performance of Different Models')
+if x_steps:
+    plt.xlabel('Steps')
+else:
+    plt.xlabel('Grasping Points')
+
 plt.ylabel('Accuracy')
 
 plt.legend()
+if x_steps:
+    plt.savefig('plots_plakat/temp/performance_plot_steps.png')
+else:
+    plt.savefig('plots_plakat/temp/performance_plot_gps.png')
 
-plt.savefig('plots_plakat/temp/performance_plot.png')
+
 plt.show()
