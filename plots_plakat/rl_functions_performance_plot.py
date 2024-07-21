0519e5e0960f373c8f1a44be3dc32e134491a996
janrueggi <jan.rueggemeier@gmail.com>
2024-07-21 17:19:49 +0200

    some changes

--- plots_plakat/rl_functions_performance_plot.py
+++ plots_plakat/rl_functions_performance_plot.py
@@ -5,12 +5,21 @@ import time
 
 data_path = 'plots_plakat/plot_data/rl_policies_statistics.pkl'
 
+name_mask = {
+    'rl_models/rew500k9': 'Diff',
+    'rl_models/obs500k9': 'Total',
+    'random': 'Random',
+    'rl_models/allow_miss_free_ray': 'Allow_miss',
+    'rl_models/complex_after_free': 'Total_after_free',
+    'rl_models/diff_after_free': 'Diff_after_free'
+}
+
 with open(data_path, 'rb') as f:
     data = pickle.load(f)
     f.close()
 
 x = range(1, 11)
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(12, 6))
 
 for key, value in data.items():
     print(f'{key}: {value[-1,0]}')
@@ -19,12 +28,12 @@ for key, value in data.items():
 
     upper = mean + std
     lower = mean - std
-    p = plt.plot(x, mean, label=key)
+    p = plt.plot(x, mean, label=name_mask[key])
     c = p[-1].get_color()
-    plt.fill_between(x, upper, lower, color=c, alpha=0.3)
+    # plt.fill_between(x, upper, lower, color=c, alpha=0.3)
 
 
-plt.title('Example Plot of Different Datasets')
+plt.title('Reward functions')
 plt.xlabel('Steps')
 plt.ylabel('Accuracy')
 
