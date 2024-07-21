d0d7d67143dfbdde6d0531391da6da7def81875b
janrueggi <jan.rueggemeier@gmail.com>
2024-07-21 23:13:19 +0200

    plots

--- plots_plakat/rl_functions_performance_plot.py
+++ plots_plakat/rl_functions_performance_plot.py
@@ -5,7 +5,7 @@ import time
 
 from color_shemes import main_colorscheme
 
-data_path = 'plots_plakat/plot_data/rl_policies_statistics.pkl'
+data_path = 'plots_plakat/plot_data/rl_policies_statistics_gp.pkl'
 
 name_mask = {
     'rl_models/rew500k9': 'Diff',
@@ -25,20 +25,7 @@ x = range(1, 11)
 plt.figure(figsize=(8, 4))
 
 for i, [key, value] in enumerate(data.items()):
-    '''
-    if not key.endswith('metric'):
-        print(f'{key}: {value[-1,0]}')
-        mean = value[:,0]
-        std = value[:,1]
-
-        upper = mean + std
-        lower = mean - std
-        c = main_colorscheme[i]
-        p = plt.plot(x, mean, label=key, color=c)
-        #plt.fill_between(x, upper, lower, color=c, alpha=0.3)
-    '''
-
-    print(f'{key}: {value[-2,0]}')
+    print(f'{key}: {value[-1,0]}')
     mean = value[:,0]
     std = value[:,1]
 
@@ -50,11 +37,10 @@ for i, [key, value] in enumerate(data.items()):
 
 plt.ylim((0, 100))
 plt.xlim((1, 10))
-plt.title('Policies performance - 10 ray casts')
 plt.xlabel('Steps')
 plt.ylabel('Accuracy')
 
 plt.legend()
 
-plt.savefig('plots_plakat/temp/performance_plot.png', dpi=300)
+plt.savefig('plots_plakat/temp/performance_plot_gp.png')
 plt.show()
