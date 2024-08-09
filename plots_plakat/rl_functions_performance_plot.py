ef71a5fa886d4eb4c42f083a85449359eb38e47c
ge36quv <ge36quv@mytum.com>
2024-08-09 12:23:25 +0200

    commiting for pull

--- plots_plakat/rl_functions_performance_plot.py
+++ plots_plakat/rl_functions_performance_plot.py
@@ -5,15 +5,15 @@ import time
 
 from color_shemes import main_colorscheme
 
-'''
+
 x_steps = False
 
 if x_steps:
     data_path = 'plots_plakat/plot_data/rl_policies_statistics.pkl'
 else:
     data_path = 'plots_plakat/plot_data/rl_policies_statistics_gp.pkl'
-'''
-data_path = 'plots_plakat/plot_data/rl_policies_statistics_gp.pkl'
+
+#data_path = 'plots_plakat/plot_data/rl_policies_statistics_gp.pkl'
 
 name_mask = {
     'rl_models/rew500k9': 'Diff',
@@ -29,23 +29,23 @@ with open(data_path, 'rb') as f:
     data = pickle.load(f)
     f.close()
 
-x = range(1, 11)
-plt.figure(figsize=(12, 6))
 
-for i, [key, value] in enumerate(data.items()):
-    '''
+plt.figure(figsize=(12, 6))
+i = 0
+for key, value in data.items():
+
     if not key.endswith('metric'):
         print(f'{key}: {value[-1,0]}')
         mean = value[:,0]
         std = value[:,1]
-
+        x = range(1, len(mean)+1)
         upper = mean + std
         lower = mean - std
         c = main_colorscheme[i]
         p = plt.plot(x, mean, label=key, color=c)
         #plt.fill_between(x, upper, lower, color=c, alpha=0.3)
+        i += 1
     '''
-
     print(f'{key}: {value[-2,0]}')
     mean = value[:,0]
     std = value[:,1]
@@ -53,9 +53,9 @@ for i, [key, value] in enumerate(data.items()):
     upper = mean + std
     lower = mean - std
     c = main_colorscheme[i]
-    p = plt.plot(x, mean, label=name_mask[key]), color=c)
+    p = plt.plot(x, mean, label=name_mask[key], color=c)
     #plt.fill_between(x, upper, lower, color=c, alpha=0.3)
-
+    '''
 plt.title('Performance of Different Models')
 if x_steps:
     plt.xlabel('Steps')
@@ -76,5 +76,5 @@ if x_steps:
 else:
     plt.savefig('plots_plakat/temp/performance_plot_gps.png')
 
-
+print("show")
 plt.show()
